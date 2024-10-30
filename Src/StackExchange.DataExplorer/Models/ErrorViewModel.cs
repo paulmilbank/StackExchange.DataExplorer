fe6f2342a8862cb82154179be43311408d28ff1b
Paul Milbank <paul.milbank@pushpay.com>
2024-10-31 08:02:05 +1300

    Initial scaffolding for Dotnet 8 projects

---
+++ Src/StackExchange.DataExplorer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StackExchange.DataExplorer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
